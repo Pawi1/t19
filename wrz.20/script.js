9d24e14daf095ff4067c76226378d696e0740e2a
Pawi1 <p.rutkow@o2.pl>
2023-09-20 09:27:34 +0200

    dodanie 20 wrz

---
+++ wrz.20/script.js
@@ -0,0 +1,20 @@
+function mnozenie(){
+    a = prompt("Podaj a:","");
+    b = prompt("Podaj b:","");
+    alert(`${a}*${b} = ${a*b}`);
+}
+function dzielenie(){
+    a = prompt("Podaj a:","");
+    b = prompt("Podaj b:","");
+    alert(`${a}:${b} = ${a/b}`);
+}
+function dodawanie(){
+    a = prompt("Podaj a:","");
+    b = prompt("Podaj b:","");
+    alert(`${a}+${b} = ${a- -b}`);
+}
+function odejmanie(){
+    a = prompt("Podaj a:","");
+    b = prompt("Podaj b:","");
+    alert(`${a}-${b} = ${a-b}`);
+}
