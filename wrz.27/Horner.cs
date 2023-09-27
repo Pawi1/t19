eaa56088408610e151d24fc92463cbf1c4c62b1e
Paweł Rutkowski <114557817+Pawi1@users.noreply.github.com>
2023-09-27 13:53:08 +0200

    dodanie Horner

---
+++ wrz.27/Horner.cs
@@ -0,0 +1,11 @@
+﻿int Horner(int systemLiczbowy,string liczba)
+{
+	int wynik = liczba[0]-48;
+	for (int i = 1;i<liczba.Length;i++)
+	{
+		int temp = wynik; 
+		wynik = systemLiczbowy * temp + liczba[i]-48;
+	}
+	return wynik;
+}
+Console.WriteLine("Wynik: "+Horner(4, "3333"));
