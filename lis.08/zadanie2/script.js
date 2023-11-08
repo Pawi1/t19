db2a91d40019a3fa3ae628c9d20a0b7d920945c7
pawi1 <p.rutkow@o2.pl>
2023-11-08 11:50:40 +0100

    dodanie zadanie 2

--- lis.08/zadanie2/script.js
+++ lis.08/zadanie2/script.js
@@ -1,10 +1,21 @@
 function funkcja()
 {
-let f = document.getElementsByName("radio");
-let v;
-for(var i = 0;f.length>i;i++)
-{
-    if(f[i].checked) v = f[i].value;
+    let f = document.getElementsByName("radio");
+    let v;
+    for(var i = 0;f.length>i;i++)
+    {
+        if(f[i].checked) v = f[i].value;
+    }
+    alert(v);
 }
-alert(v);
+function funkcja2()
+{
+    let f = document.getElementsByName("ciastka");
+    let v = "";
+    for(var i = 0;f.length>i;i++)
+    {
+        if(f[i].checked) v += f[i].value + " ";
+    }
+    if(v=="") alert("Nic nie wybrano");
+    else alert(`Masz ochote na: ${v}`);
 }
