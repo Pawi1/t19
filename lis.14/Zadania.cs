dc0deb61ae4a542d500788be6cc2a46233580b83
pawi1 <p.rutkow@o2.pl>
2023-11-14 12:11:51 +0100

    dodanie zabezpieczeń

--- lis.14/Zadania.cs
+++ lis.14/Zadania.cs
@@ -11,8 +11,17 @@ public class Zadanie1
     {
         Console.Write("Podaj nazwę nowej tablicy: ");
         string arrayName = Console.ReadLine()??"";
-        Console.Write($"Podaj rozmiar tablicy {arrayName}: ");
-        int size = Convert.ToInt32(Console.ReadLine());
+        int size;
+        while (true)
+        {
+            Console.Write($"Podaj rozmiar tablicy {arrayName}: ");
+            if (!int.TryParse(Console.ReadLine(), out size))
+            {
+                Console.WriteLine("Podano złą wartość");
+                continue;
+            }
+            break;
+        }
         return (new int[size],arrayName);
     }
     static void SetArray((int[] Table,string name) Array)
@@ -20,17 +29,20 @@ public class Zadanie1
         Console.WriteLine($"Podaj wartości które mają zostać dodane do tablicy {Array.name}, tablica ma określoną długość, w tym przypadku {Array.Table.Length}");
         for (int i = 0;i<Array.Table.Length;i++)
         {
-         Console.Write($"Podaj wartość [{i+1}/{Array.Table.Length}]: ");
-         Array.Table[i] = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Podaj wartość [{i+1}/{Array.Table.Length}]: ");
+            if (!int.TryParse(Console.ReadLine(), out int temp))
+            {
+                Console.WriteLine("Podano złą wartość");
+                i--;
+                continue;
+            }
+            Array.Table[i] = temp;
         }
     }
 
     static void DisplayArray((int[] Table,string name) Array)
     {
         Console.WriteLine($"Tablica {Array.name} wygląda następująco:");
-        foreach (int number in Array.Table)
-        {
-            Console.Write($"{number} ");
-        }
+        foreach (int number in Array.Table) Console.Write($"{number} ");
     }
 }
