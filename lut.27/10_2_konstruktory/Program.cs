ce418e43b0e3abbf63b998f412bb243680ea854a
Pawil <p.rutkow@o2.pl>
2024-02-27 11:22:37 +0100

    27.02

---
+++ lut.27/10_2_konstruktory/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _10_2_konstruktory;
+internal class Program{
+static void Main()
+    {
+        Person p1 = new();
+        Console.WriteLine(p1.GetData());
+        Console.WriteLine(Person.Counter);
+    }
+}
---
+++ lut.27/10_2_konstruktory/classes/Person.cs
@@ -0,0 +1,28 @@
+﻿namespace _10_2_konstruktory;
+
+internal class Person
+{
+    public static int Counter = 0;
+    public string Name { get;set; }
+    public string Surname { get; set; }
+    
+    public int Age {get;set;}
+    static Person()
+    {
+        Console.WriteLine("Statyczny konstruktor klasy Person");
+        Counter++;
+    }
+    public Person(){
+        Name = "nieznane";
+        Surname = "nieznane";
+    }
+    public Person(string name,string surname)
+    {
+        Name = name;
+        Surname = surname;
+    }
+    public string GetData()
+    {
+        return $"Imie i nazwisko {Name} {Surname}, wiek {Age}";
+    }
+}
