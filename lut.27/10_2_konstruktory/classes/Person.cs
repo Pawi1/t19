1deef60856ba76e7c560938f5ed83595931baa38
Pawil <p.rutkow@o2.pl>
2024-02-28 12:37:52 +0100

    t19

--- lut.27/10_2_konstruktory/classes/Person.cs
+++ lut.27/10_2_konstruktory/classes/Person.cs
@@ -7,20 +7,32 @@ internal class Person
     public string Surname { get; set; }
     
     public int Age {get;set;}
+    public float Height {get;set;}
     static Person()
     {
         Console.WriteLine("Statyczny konstruktor klasy Person");
         Counter++;
     }
     public Person(){
-        Name = "nieznane";
-        Surname = "nieznane";
+        Name = "";
+        Surname = "";
+        Counter++;
+    }
+    public Person(string name)
+    {
+        Name = name;
+        Surname = "";
+        Counter++;
     }
     public Person(string name,string surname)
     {
         Name = name;
         Surname = surname;
+        Counter++;
     }
+    public Person(string name,string surname, int age):this(name,surname) {Age = age; Counter++;}
+
+    public Person(string name, string surname, int age, float height):this(name,surname,age){Height = height; Counter++;}
     public string GetData()
     {
         return $"Imie i nazwisko {Name} {Surname}, wiek {Age}";
