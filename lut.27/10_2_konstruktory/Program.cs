1367ad2f0ec189191affece4877c48321fc92041
Pawil <p.rutkow@o2.pl>
2024-02-28 12:40:41 +0100

    t19

--- lut.27/10_2_konstruktory/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace _10_2_konstruktory;
-internal class Program{
-static void Main()
-    {
-        Person p1 = new();
-        Console.WriteLine(p1.GetData());
-        Console.WriteLine(Person.Counter);
-    }
-}
