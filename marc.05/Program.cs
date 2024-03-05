77b4ff09ae3f6c179a5b7af6a78be3c8200bd47e
Pawil <p.rutkow@o2.pl>
2024-03-05 11:25:36 +0100

    t19

--- marc.05/Program.cs
+++ marc.05/Program.cs
@@ -2,10 +2,57 @@
 
 class Program
     {
-        static void Main(string[] args)
+        static void Main()
             {
-                Animal a = new Animal("Burek");
-                
-                Console.ReadKey();
+                List<Animal> animals = [];
+                ShowMainMenu(animals);
             }
-    }
+        private static void ShowMainMenu(List<Animal> animals)
+            {
+                Console.Clear();
+                Console.WriteLine("Witaj w programie do zarządzania zwierzętami");
+                Console.WriteLine("\t1. Dodaj zwierze");
+                Console.WriteLine("\t2. Pokaż liste zwierząt");
+                Console.WriteLine("\t3. Pokaż szczegóły zwierze");
+                Console.WriteLine("\t4. Usuń zwierze");
+                Console.WriteLine("\t5. Zakończ program\n");
+                Console.Write("Wybierz jedną z opcji: ");
+                string choice = Console.ReadLine()??"";
+                switch(choice)
+                    {
+                        case "1":
+                            AddNewAnimal(animals);
+                            break;
+                        case "2":
+                            ShowAnimalList(animals);
+                            break;
+                        case "3":
+                            ShowAnimalDetails(animals);
+                            break;
+                        case "4":
+                            RemoveAnimal(animals);
+                            break;
+                        case "5":
+                            break;
+                    }
+            }
+
+    private static void RemoveAnimal(List<Animal> animals)
+        {
+        
+        }
+
+    private static void ShowAnimalDetails(List<Animal> animals)
+        {
+        
+        }
+
+    private static void AddNewAnimal(List<Animal> animals)
+        {
+        
+        }
+    private static void ShowAnimalList(List<Animal> animals)
+        {
+        
+        }
+}
--- marc.05/classes/Animal.cs
+++ marc.05/classes/Animal.cs
@@ -10,30 +10,33 @@ public enum Kind
     }
 internal class Animal
     {
-        public string Name { get;set; }
-        public DateTime BirthDate { get;set; }
-        public bool IsMammal { get;set; }
-        public Kind Kind { get;set; }
+        public string Name { get;set; } = "";
+        public DateTime BirthDate { get;set; } = DateTime.MinValue;
+        public bool IsMammal { get;set; } = true;
+        public Kind Kind { get;set; } = Kind.Ssak;
 
         public Animal(string name)
             {
                 Name = name;
-                BirthDate = DateTime.MinValue; // Domyślna data urodzenia
-                IsMammal = true;
-                Kind = Kind.Ssak;
+            }
+        public Animal(string name, DateTime birthDate):this(name)
+            {
+                BirthDate = birthDate;
+            }
+        public Animal(string name, DateTime birthDate, bool isMammal):this(name,birthDate)
+            {
+                IsMammal = isMammal;
+            }
+        public Animal(string name,DateTime birthDate, bool isMammal, Kind kind):this(name,birthDate,isMammal)
+            {
+                Kind = kind;
             }
         public string Describe()
             {
-                string description = string.Concat("Nazwa zwierzęcia: ",Name,". Data urodzenia: ",BirthDate.ToShortDateString(),"r.");
-                if(IsMammal)
-                    {
-                        description = string.Concat(description," Zwierze jest ssakiem. ");
-                    }
-                else
-                    {
-                        description = string.Concat(description," Zwierze nie jest ssakiem. ");
-                    }
-                description = string.Concat(description,"Rodzaj zwierzęcia: ",Kind);
-                return description;
+                return string.Concat("Nazwa zwierzęcia: ",Name,". Data urodzenia: ",BirthDate.ToShortDateString(),"r.",IsMammal?" Zwierze jest ssakiem. ":" Zwierze nie jest ssakiem. ","Rodzaj zwierzęcia: ",Kind);
+            }
+        public string ShowAge()
+            {
+                return string.Concat(Name," ma ",DateTime.Now.Year-BirthDate.Year," lat.");
             }
     }
