af22dfa332330e1f7035c59135429c5b6819d611
Pawil <p.rutkow@o2.pl>
2024-03-06 15:05:29 +0100

    t19

---
+++ marc.06/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace marc._06;
+
+class Program
+    {
+        static void Main()
+            {
+                var G = new Dictionary<int,List<int>>();
+                Console.Write("Podaj ilosc wierzołków: ");
+                var n = int.Parse(Console.ReadLine()??"");
+                for (int i = 1; i <= n; i++)
+                    {
+                        G.Add(i,[]);
+                    }
+                Console.Write("Podaj ilość krawędzi: ");
+                int k = int.Parse(Console.ReadLine()??"");
+        
+                for (int i = 0;i<k;i++)
+                    {
+                        Console.Write("Podaj połączenie (x-y): ");
+                        string[] j = (Console.ReadLine()??"").Split("-");
+                        G[int.Parse(j[0])].Add(int.Parse(j[1]));
+                    }
+                foreach (var item in G)
+                    {
+                        Console.Write(item.Key + " - ");
+                        foreach (var item1 in item.Value)
+                            {
+                                Console.Write(item1 + " ");
+                            }
+                        Console.WriteLine();
+                    }
+            // obliczenie sumy wierzcholków grafu
+                
+            // podaj ilość wierzchołków bez sąsiadów
+            // sprawdź czy isnieje krawędź między a i b
+            }
+    }
