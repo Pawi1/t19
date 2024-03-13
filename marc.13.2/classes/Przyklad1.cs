e9cb9b10a7b37d673f832ff6a02ed14f54869229
Pawil <p.rutkow@o2.pl>
2024-03-13 14:16:33 +0100

    t19

---
+++ marc.13.2/classes/Przyklad1.cs
@@ -0,0 +1,15 @@
+﻿namespace marc._13._2;
+
+public class Przyklad1
+{
+    // pola
+    private int wiek=0;
+    // public private protected internal
+    // właściwości
+    public int wzrost { get; set; }
+    // metody
+    public void metoda1() {}
+    public int getWiek() { return wiek; }
+    public void setWiek(int wiek) { this.wiek = wiek; }
+
+}
