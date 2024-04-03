12179514278527353de4241a67c226d435abc2ea
Pawi1 <p.rutkow@o2.pl>
2024-04-03 14:46:50 +0200

    t19

--- kwi.03/composite/Program.cs
+++ kwi.03/composite/Program.cs
@@ -1,9 +1,25 @@
 ﻿namespace composite;
 
+public abstract class Component 
+{
+    public abstract void Operation();
+}
+public class Leaf : Component
+{
+    public override void Operation(){}
+}
+public class Composite : Component
+{
+    public List<Component> components = new();
+    public override void Operation(){}
+    
+    public void Add(){}
+    public void Remove(){}
+    public void GetChild(){}
+}
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
     }
 }
