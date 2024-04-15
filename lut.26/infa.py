36eb39c38ce5a2f43733a9605bbd109968f332e2
Pawi1 <p.rutkow@o2.pl>
2024-04-15 07:42:56 +0200

    t19

--- lut.26/infa.py
+++ lut.26/infa.py
@@ -12,14 +12,14 @@ def prostokaty(a,b,n):
 
 def prostokaty2(a,b,n):
     dx = (b-a)/n
-    s =0
+    s = 0
     for i in range(n):
         s += f(a+dx/2+i*dx) * dx
     return s
 
 def prostokaty3(a,b,n):
     dx = (b-a)/n
-    s =0
+    s = 0
     for i in range(n):
         s += f(a+dx+i*dx) * dx
     return s
--- marc.11/bisekcja.py
+++ marc.11/bisekcja.py
@@ -1,3 +1,13 @@
-def bisekcja(n):
+def f(x):
+    return x+1
+
+def bisekcja(a,b,n):
     for i in range(n):
-        i
+        c = (a + b) / 2
+        if f(c) == 0:
+            return c
+        if f(a) * f(c) < 0:
+            b = c
+        else:
+            a = c
+    return (a+b)/2
