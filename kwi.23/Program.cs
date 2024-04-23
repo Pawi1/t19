78ae6d35025813eb37a7c02fcbf0cdd8466151d9
Pawi1 <p.rutkow@o2.pl>
2024-04-23 12:17:00 +0200

    t19

--- kwi.23/Program.cs
+++ kwi.23/Program.cs
@@ -2,11 +2,19 @@
 
 class Program
 {
+    public enum FuelType
+    {
+        Petrol,
+        Diesel,
+        Electric
+    }
     public class Vehicle
     {
         public string Brand { get; set; } = "???";
         public string Model { get; set; } = "???";
         public double Fuel { get; private set; }
+        public FuelType FuelType { get; set; }
+        public ushort Speed { get; set; }
         public virtual void StartEngine()
         {
             Console.WriteLine($"Silnik pojazdu: {Brand} {Model} został uruchomiony…");
@@ -18,7 +26,10 @@ class Program
     }
     public class Car : Vehicle
     {
-        public sbyte NumberOfDoors { get; set; }
+        public byte NumberOfDoors { get; set; }
+        public byte CurrentGear { get; set; }
+        public byte MaxGear { get; set; }
+        public bool IsAutomatic { get; set; }
         public override void StartEngine()
         {
             //base.StartEngine();
@@ -28,6 +39,40 @@ class Program
         {
             //base.StopEngine();
             Console.WriteLine($"Silnik samochodu: {Brand} {Model} został zatrzymany…");
+        }
+        public void ChangeGear(sbyte gear)
+        {
+            if (!IsAutomatic)
+            {
+                if (gear < 0 || gear > MaxGear)
+                {
+                    Console.WriteLine("Nieprawidlowy bieg");
+                }
+                else
+                {
+                    CurrentGear++;
+                    Console.WriteLine("Aktualny bieg: "+CurrentGear);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Samochód ma automatyczną skrzynie biegów…");
+            }
+        }
+        public void AutomaticChangeGear()
+        {
+            if(Speed < 30)
+            {
+                CurrentGear = 1;
+            }
+            else if(Speed < 70)
+            {
+                CurrentGear = 2;
+            }
+            else
+            {
+                CurrentGear = 3;
+            }
         }
     }
     static void Main(string[] args)
