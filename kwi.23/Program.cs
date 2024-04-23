f267c81ed0d61614b1583663f68d36590d37375a
Pawi1 <p.rutkow@o2.pl>
2024-04-23 10:29:38 +0200

    t19

---
+++ kwi.23/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace kwi._23;
+
+class Program
+{
+    public class Vehicle
+    {
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+        public virtual void StartEngine()
+        {
+            Console.WriteLine($"Silnik od pojazdu: {Brand} {Model} został uruchomiony…");
+        }
+    }
+    public class Car : Vehicle { }
+    static void Main(string[] args)
+    {
+        Vehicle vehicle = new();
+        vehicle.Brand = "Audi";
+        Console.WriteLine(vehicle.Brand);
+        vehicle.StartEngine();
+        Car car = new();
+        car.Brand = "Fiat";
+        Console.WriteLine(car.Brand);
+        car.StartEngine();
+        Console.ReadKey();
+    }
+}
