e8833782e2e55e46566995969bddb11fd70d8a74
pawi1 <p.rutkow@o2.pl>
2024-10-02 11:19:06 +0200

    t19

---
+++ 2024.09.25/zadania/Form3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadania
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(DateTime.Now.ToString("MMMM yyyy"));
+            MessageBox.Show(DateTime.Now.ToString("T"));
+        }
+    }
+}
