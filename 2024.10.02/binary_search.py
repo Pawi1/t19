52405f27eedd1930e5f9ec493da27caa01a412ac
pawi1 <p.rutkow@o2.pl>
2024-10-09 15:07:57 +0200

    t19

--- 2024.10.02/binary_search.py
+++ 2024.10.02/binary_search.py
@@ -15,7 +15,7 @@ Wartość Prawda, gdy a należy do tablicy T, Fałsz w przeciwnym przypadku
 """
 
 
-def binarySearch(T: list, a: int) -> bool:
+def binarySearch(T: list[int], a: int) -> bool:
     n = len(T)
     left = 0
     right = n - 1
@@ -64,7 +64,7 @@ Wykonaj zadanie 1 wykorzystując wyszukiwanie binarne w wersji rekurencyjnej.
 """
 
 
-def binarySearchreku(T: list, a: int, left: int = 0, right: int = -1) -> bool:
+def binarySearchreku(T: list[int], a: int, left: int = 0, right: int = -1) -> bool:
     if right == -1:
         right = len(T) - 1
     if left > right:
@@ -97,4 +97,45 @@ file.close()
 for sequence in sequences:
     if sequence == "":
         break
-    print(map(list, sequence))
+    if binarySearch(list(map(int, sequence.split())), 10):
+        print(sequence)
+"""
+Zadanie 4.
+Napisz program, który wyświetli na standardowym wyjściu te ciągi liczbowe z pliku ciagi2.txt, które zawierają liczbę 10. Wykorzystaj wyszukiwanie binarne. W pliku ciagi2.txt:
+Pierwszy wiersz – liczba ciągów w pliku
+Drugi wiersz – ile jest liczb w kolejnym wierszu
+Trzeci wiersz – pierwszy ciąg, który ma tyle liczb rozdzielonych spacją ile wskazuje liczba w poprzednim wierszu.
+Czwarty wiersz – ile jest liczb w kolejnym wierszu
+Piąty wiersz – drugi ciąg, który ma tyle liczb rozdzielonych spacją ile wskazuje liczba w poprzednim wierszu.
+itd.
+"""
+file = open("ciagi2.txt")
+n = int(file.readline().rstrip())
+for i in range(n):
+    d = int(file.readline().strip())
+    sequence = list(map(int, file.readline().split()))
+    if binarySearch(sequence, 10):
+        print(sequence)
+file.close()
+"""
+Zadanie 5.
+Napisz algorytm w formie pseudokodu przyjmując, że elementy tablicy T indeksowane są od 1.
+Specyfikacja:
+Dane:
+a – liczba całkowita
+n – liczba całkowita dodatnia n oznaczającą liczbę elementów w tablicy T
+T[1..n] – tablica zawierająca n liczb uporządkowanych niemalejąco
+Wyniki:
+Wartość Prawda, gdy a należy do tablicy T, Fałsz w przeciwnym przypadku
+"""
+"""pseudocode
+
+"""
+"""
+Zadanie 6.
+Napisz program, który wylosuje 1 000 000 liczb w kolejności rosnącej. 
+Wylosuj pierwszą liczbę z przedziału od 1 do 10, a każdą następną dodając do poprzedniej liczby liczbę losową od 1 do 3. 
+Następnie sprawdź, czy wśród wylosowanych liczb jest liczba 1 500 000, zapisując w pliku zadanie4.txt jeden z komunikatów „tak” lub „nie” 
+w zależności od tego, czy ta liczba jest, czy nie jest wśród wylosowanych. Ponadto wyświetl w nowej linii liczbę porównań z liczbą 1 500 000.
+"""
+# from random import randint
