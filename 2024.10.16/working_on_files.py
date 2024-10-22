60de08f7c7a438aae38d5b7a35595ed764537312
pawi1 <p.rutkow@o2.pl>
2024-10-22 11:22:27 +0200

    t19

--- 2024.10.16/working_on_files.py
+++ 2024.10.16/working_on_files.py
@@ -13,7 +13,6 @@ user_surname: str = input("Podaj nazwisko: ")
 file = open("dane_osobowe.txt", "w")
 file.write(user_name + "\n" + user_surname)
 file.close()
-file = None
 """
 Zadanie 2.
 Napisz program, który odczyta imię i nazwisko zapisane w pliku dane_osobowe.txt
@@ -24,7 +23,6 @@ file = open("dane_osobowe.txt")
 user_info = file.read().rstrip().split()
 print(f"Witaj {user_info[0]} {user_info[1]}")
 file.close()
-file = None
 """
 Zadanie 3.
 Napisz program, który:
@@ -38,8 +36,56 @@ for i in range(10):
     file.write(str(randint(1, 100)) + "\n")
 file.close()
 file = open("losowe.txt")
-numbers = map(int, file.read().rstrip().split())
+numbers = list(map(int, file.read().rstrip().split()))
 print(sum(numbers))
 print(min(numbers))
 print(max(numbers))
 print((min(numbers) + max(numbers)) / 100)
+file.close()
+"""
+Zadanie 4.
+Napisz program, który wyświetli liczbę ciągów z pliku ciagi.txt, 
+które mają sumę wyrazów parzystą. Opis zawartości pliku ciagi.txt znajduje się 
+w przykładzie 6.
+"""
+file = open("ciagi.txt")
+sum_of_even: int = 0
+for line in file:
+    sequence: list[int] = list[int](map(int, line.split()))
+    if sum(sequence) % 2 == 0:
+        sum_of_even += 1
+print(sum_of_even)
+file.close()
+"""
+Zadanie 5.
+Napisz program (każdy podpunkt jako osobny program), który odczyta plik slowa.txt, a następnie:
+- wyświetli ponumerowane słowa z pliku tekstowego
+- wyświetli liczbę słów w pliku
+- wyświetli słowa zaczynające się na literę A
+- wyświetli słowa kończące się na literę A
+- wyświetli słowa oraz liczbę liter, z których się składają
+- *wyświetli najkrótsze oraz najdłuższe słowo w pliku oraz ich długość (jeżeli jest ich kilka wyświetl pierwsze z nich)
+- wyświetli słowa o długości 6
+- wyświetli słowa zawierające literę O oraz dla każdego z tych słów liczbę tych liter O
+- wyświetli ile razy w całym pliku występuje litera A
+"""
+with open("slowa.txt") as file:
+    i: int = 1
+    for line in file:
+        print(str(i) + " " + line.rstrip())
+        i = +1
+"""
+- wyświetli liczbę słów w pliku
+"""
+with open("slowa.txt") as file:
+    i: int = 1
+    for line in file:
+        i = +1
+    print(i)
+"""
+- wyświetli słowa zaczynające się na literę A
+"""
+with open("slowa.txt") as file:
+    for line in file:
+        if line[0] == "A":
+            print(line)
