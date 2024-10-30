bd730f443e1688b7917edf15fdece046caadc199
pawi1 <p.rutkow@o2.pl>
2024-10-30 15:39:59 +0100

    t19

--- 2024.10.30/merge_sort.py
+++ 2024.10.30/merge_sort.py
@@ -1,19 +1,24 @@
 """
 Merge Sort
 """
+
 """
 Zadanie 1.
 Napisz program, który wczyta z klawiatury ciąg liczb rozdzielonych spacjami, a 
 następnie posortuje ten ciąg za pomocą algorytmu przedstawionego w przykładzie i wyświetli po posortowaniu.
 """
-sequence:list[int] = list(map(int,input('Podaj ciąg liczb: ').split()))
+sequence: list[int] = [int(i) for i in input("Podaj ciąg liczb: ").split()]
 
-help_list:list[int] = [0 in range(len(sequence)-1)]
-def merge(t_list:list[int],left:int, middle:int, right:int,help_list:list[int])->None:
-    i:int = left
-    j:int = middle + 1
-    k:int = left
-    while i<=middle and j<=right:
+help_list: list[int] = [0] * len(sequence)
+
+
+def merge(
+    t_list: list[int], left: int, middle: int, right: int, help_list: list[int]
+) -> None:
+    i: int = left
+    j: int = middle + 1
+    k: int = left
+    while i <= middle and j <= right:
         if t_list[i] < t_list[j]:
             help_list[k] = t_list[i]
             i += 1
@@ -21,23 +26,25 @@ def merge(t_list:list[int],left:int, middle:int, right:int,help_list:list[int])-
             help_list[k] = t_list[j]
             j += 1
         k += 1
-    while i<=middle:
+    while i <= middle:
         help_list[k] = t_list[i]
         i += 1
         k += 1
-    while j<=right:
+    while j <= right:
         help_list[k] = t_list[j]
         j += 1
         k += 1
-    for i in range(left,right):
+    for i in range(left, right + 1):
         t_list[i] = help_list[i]
 
-def sort(t_list:list[int],left:int,right:int):
-    if left < right:
-        middle = (left+right)//2
-        sort(t_list,left,middle)
-        sort(t_list,middle+1,right)
-        merge(t_list,left,middle,right,help_list)
 
-sort(sequence,0,len(sequence)-1)
+def sort(t_list: list[int], left: int, right: int):
+    if left < right:
+        middle = (left + right) // 2
+        sort(t_list, left, middle)
+        sort(t_list, middle + 1, right)
+        merge(t_list, left, middle, right, help_list)
+
+
+sort(sequence, 0, len(sequence) - 1)
 print(sequence)
