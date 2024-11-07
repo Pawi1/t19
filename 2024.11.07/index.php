b1be2337cdef243ceb2ae1841799ee0080e1aa1c
pawi1 <p.rutkow@o2.pl>
2024-11-07 08:40:28 +0100

    t19

---
+++ 2024.11.07/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>zadanie 1</title>
+</head>
+<body>
+    <form action="" method="get">
+        <label for="tekst">Podaj tekst: </label>
+        <input type="text" name="tekst" id="tekst">
+        <button type="submit">Przetwórz</button>
+    </form>
+    <p id="output"><?php echo $_GET["tekst"] ?></p>
+</body>
+</html>
