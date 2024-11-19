133a191c9e71e2e71ec5889b1acf2b2ccfaa4d4f
pawi1 <p.rutkow@o2.pl>
2024-11-19 11:17:01 +0100

    t19

--- informatyka/2024.11.12/horners_method.py
+++ informatyka/2024.11.12/horners_method.py
@@ -7,45 +7,94 @@ Napisz program w języku Python, który obliczy wartość wielomianu podanego pr
 naiwny. Użytkownik podaje współczynniki wielomianu jako liczby rozdzielone spacjami, 
 przy czym pierwsza liczba oznacza wyraz wolny a0, a ostatnia współczynnik przy najwyższej potędze.
 """
-def polynomial(l:list[int], x:float)->float:
-    n:int = len(l) - 1
-    y:float = l[0]
-    power:float = 1
-    for i in range(1,n+1):
+
+
+def polynomial(l: list[int], x: float) -> float:
+    n: int = len(l) - 1
+    y: float = l[0]
+    power: float = 1
+    for i in range(1, n + 1):
         power *= x
         y = y + l[i] * power
     return y
-user_list:list[int] = list(map(int,input('Podaj współczynniki rozdzielone spacjami: ').split()))
+
+
+user_list: list[int] = list(map(int, input('Podaj współczynniki rozdzielone spacjami: ').split()))
 user_x = float(input('Podaj x: '))
-print(polynomial(user_list,user_x))
+print(polynomial(user_list, user_x))
 """
 Zadanie 2
 Napisz program w języku Python, który obliczy wartość wielomianu podanego przez użytkownika, wykorzystując
 schemat Hornera w wersji iteracyjnej (zgodnie z przykładem 2). Użytkownik podaje współczynniki wielomianu jako liczby
 rozdzielone spacjami, przy czym pierwsza liczba oznacza wyraz wolny a0, a ostatnia współczynnik przy najwyższej potędze.
 """
-def horner(l:list[int],x:float)->float:
-    n:int = len(l) - 1
-    y:float = l[n]
-    for i in range(n-1,-1,-1):
+
+
+def horner(l: list[int], x: float) -> float:
+    n: int = len(l) - 1
+    y: float = l[n]
+    for i in range(n - 1, -1, -1):
         y *= x
         y += l[i]
     return y
-user_list:list[int] = list(map(int,input('Podaj współczynniki rozdzielone spacjami: ').split()))
+
+
+user_list: list[int] = list(map(int, input('Podaj współczynniki rozdzielone spacjami: ').split()))
 user_x = float(input('Podaj x: '))
-print(horner(user_list,user_x))
+print(horner(user_list, user_x))
 """
 Zadanie 3
 Napisz program w języku Python, który obliczy wartość wielomianu podanego przez użytkownika, wykorzystując schemat Hornera
 w wersji rekurencyjnej. Użytkownik podaje współczynniki wielomianu jako liczby rozdzielone spacjami, przy czym pierwsza liczba
 oznacza` wyraz wolny a0, a ostatnia współczynnik przy najwyższej potędze.
 """
-def horner_recursive(l:list[int],x:float)->float:
+
+
+def horner_recursive(l: list[int], x: float) -> float:
     n = len(l) - 1
     if n == 0:
         return l[0]
     return x * horner_recursive(l[1:], x) + l[0]
 
-user_list:list[int] = list(map(int,input('Podaj współczynniki rozdzielone spacjami: ').split()))
+
+user_list: list[int] = list(map(int, input('Podaj współczynniki rozdzielone spacjami: ').split()))
 user_x = float(input('Podaj x: '))
-print(horner_recursive(user_list,user_x))
+print(horner_recursive(user_list, user_x))
+"""
+Zadanie 5
+"""
+
+
+def reverse_polynomial(l: list[int], x: float) -> float:
+    n: int = len(l) - 1
+    y: float = l[n]
+    power: float = 1
+    for i in range(n - 1, -1, -1):
+        power *= x
+        y = y + l[i] * power
+    return y
+
+
+user_list: list[int] = list(
+    map(int, input('Podaj współczynniki (w kolejności malejąco) rozdzielone spacjami: ').split()))
+user_x = float(input('Podaj x: '))
+print(reverse_polynomial(user_list, user_x))
+"""
+"""
+
+
+def reverse_horner(l: list[int], x: float) -> float:
+    n: int = len(l) - 1
+    y: float = l[0]
+    for i in range(1, n):
+        y *= x
+        y += l[i]
+    return y
+
+
+user_list: list[int] = list(
+    map(int, input('Podaj współczynniki (w kolejności malejąco) rozdzielone spacjami: ').split()))
+user_x = float(input('Podaj x: '))
+print(reverse_horner(user_list, user_x))
+"""
+"""
