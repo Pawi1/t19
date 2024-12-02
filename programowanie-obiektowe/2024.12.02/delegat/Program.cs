3352e074390dd142214ad7de0029bc78481750a9
pawi1 <p.rutkow@o2.pl>
2024-12-02 15:54:18 +0100

    t19

--- programowanie-obiektowe/2024.12.02/delegat/Program.cs
+++ programowanie-obiektowe/2024.12.02/delegat/Program.cs
@@ -4,7 +4,7 @@
     {
         public void SendSms(string message)
         {
-            Console.WriteLine("Send SMS: " + message);
+            Console.WriteLine("SMS: " + message);
         }
     }
 
@@ -12,7 +12,7 @@
     {
         public void SendEmail(string message)
         {
-            Console.WriteLine("Send Email: " + message);
+            Console.WriteLine("Email: " + message);
         }
     }
 
@@ -20,7 +20,7 @@
     {
         public void SendPushNotification(string message)
         {
-            Console.WriteLine("Send Push Notification: " + message);
+            Console.WriteLine("Push Notification: " + message);
         }
     }
 
@@ -40,7 +40,10 @@
 
         public void SendNotification(string message)
         {
-            _notify?.Invoke(message);
+            if (_notify != null)
+                _notify?.Invoke(message);
+            else
+                Console.WriteLine("Notification method not implemented");
         }
     }
 
@@ -48,12 +51,99 @@
 
     public static class Program
     {
+        private static void ShowMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("~~ Menu ~~");
+            Console.WriteLine("1) Add Sms Notification");
+            Console.WriteLine("2) Add Email Notification");
+            Console.WriteLine("3) Add Push Notification");
+            Console.WriteLine("4) Remove Sms Notification");
+            Console.WriteLine("5) Remove Email Notification");
+            Console.WriteLine("6) Remove Push Notification");
+            Console.WriteLine("7) Send Notification");
+            Console.WriteLine("8) Exit");
+            Console.Write("Choose option: ");
+        }
+
+        private static string GetMessageFromConsole()
+        {
+            while (true)
+            {
+                Console.Write("Please enter the message: ");
+                var message = Console.ReadLine();
+                if (!string.IsNullOrEmpty(message))
+                    return message;
+            }
+        }
+
         private static void Main()
         {
             var emailNotifier = new EmailNotifier();
             var pushNotifier = new PushNotifier();
             var smsNotifier = new SmsNotifier();
-
+            var notificationManager = new NotificationManager();
+            while (true)
+            {
+                try
+                {
+                    ShowMenu();
+                    var option = byte.Parse(Console.ReadLine() ?? "");
+                    if (option is < 1 or > 8)
+                        throw new Exception("Invalid option: number is out of range");
+                    switch (option)
+                    {
+                        case 1:
+                            notificationManager.AddNotificationMethod(smsNotifier.SendSms);
+                            Console.WriteLine("Notification method SMS added");
+                            break;
+                        case 2:
+                            notificationManager.AddNotificationMethod(emailNotifier.SendEmail);
+                            Console.WriteLine("Notification method Email added");
+                            break;
+                        case 3:
+                            notificationManager.AddNotificationMethod(pushNotifier.SendPushNotification);
+                            Console.WriteLine("Notification method PUSH added");
+                            break;
+                        case 4:
+                            notificationManager.RemoveNotificationMethod(smsNotifier.SendSms);
+                            Console.WriteLine("Notification method SMS removed");
+                            break;
+                        case 5:
+                            notificationManager.RemoveNotificationMethod(emailNotifier.SendEmail);
+                            Console.WriteLine("Notification method Email removed");
+                            break;
+                        case 6:
+                            notificationManager.RemoveNotificationMethod(pushNotifier.SendPushNotification);
+                            Console.WriteLine("Notification method PUSH notification removed");
+                            break;
+                        case 7:
+                            notificationManager.SendNotification(GetMessageFromConsole());
+                            break;
+                        case 8:
+                            return;
+                        default:
+                            throw new Exception("Invalid option");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid option: only numbers are allowed");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Invalid option: number is too big or too small");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+                finally
+                {
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                }
+            }
         }
     }
 }
