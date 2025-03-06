de7a7715ace6738817a92d437c6214d2c50f05a7
pawi1 <p.rutkow@o2.pl>
2025-03-06 07:51:21 +0100

    t19

--- informatyka/2025.03.05/main.py
+++ informatyka/2025.03.05/main.py
@@ -1,4 +1,21 @@
 """
 Zadania powtórkowe:
 """
-# Zadanie 1
+"""
+Zadanie 6
+Napisz program, który wyświetli maksymalną sumę elementów spójnych podciągów ciągu,
+znajdującego się w pierwszym wierszu pliku ciag.txt.
+"""
+def ex6():
+    with open("ciag.txt") as file:
+        sequence: list[int] = list(map(int,file.read().split()))
+    length: int = len(sequence)
+    max_sum: int = sequence[0]
+    for seq_start in range(length):
+        for seq_length in range(1, length - seq_start + 1):
+            # print(sequence[seq_start:seq_start:seq_length])
+            seq_sum = sum(sequence[seq_start:seq_start:seq_length])
+            if max_sum < seq_sum:
+                max_sum = seq_sum
+    print(max_sum)
+# Better use Kadane's algorithm
