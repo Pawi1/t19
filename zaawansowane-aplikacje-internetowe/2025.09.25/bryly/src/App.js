ea85b34cf5f81d9f5467dbcd2c6cfb554ad5468a
pawi1 <p.rutkow@o2.pl>
2025-09-25 17:16:04 +0200

    t19

--- zaawansowane-aplikacje-internetowe/2025.09.25/bryly/src/App.js
+++ zaawansowane-aplikacje-internetowe/2025.09.25/bryly/src/App.js
@@ -6,6 +6,7 @@ let calc = new Calc()
 function App() {
   return (
     <div className="App">
+      <h1>Bryły</h1>
       <table cellPadding="0" cellSpacing="0">
         <thead>
           <tr>
@@ -19,16 +20,20 @@ function App() {
           {
             SOLID.map((figure)=>(
               <tr key ={figure.name}>
-                <td>{figure.name}</td>
+                <td><h2>{figure.name}</h2></td>
                 <td><img src={figure.image} alt='figura'></img></td>
                 <td>
-                  <p>pole: <span>{figure.formula.surfaceArea}</span></p>
-                  <p>objętość: <span>{figure.formula.volume}</span></p>
+                  <h3>pole: <span>{figure.formula.surfaceArea}</span></h3>
+                  <h3>objętość: <span>{figure.formula.volume}</span></h3>
                 </td>
                 <td>
-                  <p>wartosci: {figure.exampleDimension.toString()}</p>
-                  <p>pole: {calc.calc(figure.type,"surfaceArea",...figure.exampleDimension)?.toFixed(0)} j²</p>
-                  <p>objętość: {calc.calc(figure.type,"volume",...figure.exampleDimension)?.toFixed(0)} j³</p>
+                  <div className='dimensions'>{
+                  figure.exampleDimensions.map((dimension, index)=>(
+                    <p>{index+1}. wymiar: {dimension} j</p>
+                  ))
+                  }</div>
+                  <h3>pole: {calc.calc(figure.type,"surfaceArea",...figure.exampleDimensions)?.toFixed(0)} j²</h3>
+                  <h3>objętość: {calc.calc(figure.type,"volume",...figure.exampleDimensions)?.toFixed(0)} j³</h3>
                 </td>
               </tr>
             ))
