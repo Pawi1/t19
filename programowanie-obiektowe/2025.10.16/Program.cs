11cb9dd9fe5df0624e52ce3d7cba1d1e1d43b0ce
pawi1 <p.rutkow@o2.pl>
2025-10-16 09:25:35 +0000

    t19

---
+++ programowanie-obiektowe/2025.10.16/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _2025._10._16;
+
+public interface IPayment {
+    string Authorize();
+    string DownloadDesc();
+}
+
+public class CreditCard : IPayment
+{
+    public string Authorize() => "";
+}
+
+public class PayPal : IPayment {
+
+}
+
+
+class Program {
+    static void Main(string[] args) {
+        
+    }
+}
