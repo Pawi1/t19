e89d9e7123bb82a4a64bd870421acd00b07e92a1
Paweł Rutkowski <p.rutkow@o2.pl>
2026-09-15 11:22:26 +0200

    t19

--- informatyka/2026.09.15/main.cpp
+++ informatyka/2026.09.15/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_set>
+#include <cstdlib>
 int zad1() {
     std::vector<std::string> knowWords = {"arbuz","lokomotywa","wiatrak","krowa","mleko","ananas","osada","krab", "auto",
         "but", "cukier", "dom", "echo", "farba", "góra", "hasło", "igła", "jabłko", "kot",
@@ -42,8 +44,78 @@ int zad2() {
     }
     return 1;
 }
+std::string displayLetters(auto letters) {
+    std::string letters_s = "[";
+    for (const auto letter: letters) {
+        letters_s += std::string(1,letter) + " ";
+    }
+    letters_s.replace(letters_s.length(),1,"]");
+    return letters_s;
+}
+
+std::string uniqueLetters(std::string word) {
+    std::string letters;
+    for (auto letter: word) {
+        if (!letters.contains(letter)) {
+            letters += letter;
+        }
+    }
+    return letters;
+}
+
+void zad3() {
+    std::vector<std::string> board = {"\n\n\n\n\n\n\n\n",
+        "\n\n\n\n\n\n\n   ▬▬▬▬▬▬▬▬▬▬\n",
+        "           █\n           █\n           █\n           █\n           █\n           █\n           █\n   ▬▬▬▬▬▬▬▬▬▬\n",
+        "       ▬▬▬▬█\n           █\n           █\n           █\n           █\n           █\n           █\n   ▬▬▬▬▬▬▬▬▬▬\n",
+        "       ▬▬▬▬█\n      ○○   █\n      ○○   █\n           █\n           █\n           █\n           █\n   ▬▬▬▬▬▬▬▬▬▬\n",
+        "       ▬▬▬▬█\n      ○○   █\n      ○○   █\n      ██   █\n      ██   █\n           █\n           █\n   ▬▬▬▬▬▬▬▬▬▬\n",
+        "       ▬▬▬▬█\n      ○○   █\n      ○○   █\n     ▬██▬  █\n      ██   █\n           █\n           █\n   ▬▬▬▬▬▬▬▬▬▬\n",
+        "       ▬▬▬▬█\n      ○○   █\n      ○○   █\n     ▬██▬  █\n      ██   █\n      ▌▐   █\n           █\n   ▬▬▬▬▬▬▬▬▬▬\n"
+    };
+    std::vector<std::string> words = {"arbuz","lokomotywa","wiatrak","krowa","mleko","ananas","osada","krab", "auto",
+        "but", "cukier", "dom", "echo", "farba", "góra", "hasło", "igła", "jabłko", "kot",
+        "las", "łóżko", "miasto", "noc", "okno", "pies", "ryba", "stół", "śnieg", "telefon",
+        "ucho", "woda", "zamek", "źrebię", "żaba"};
+    std::unordered_set<char> knowLetters;
+    auto chosenWord = words[std::rand() % words.size()];
+    auto chosenWord_letters = uniqueLetters(chosenWord);
+    auto badAnswers= 0;
+    while (badAnswers < chosenWord_letters.length()) {
+        auto guess = false;
+        std::string newLetters;
+
+        std::cout << "\tWisielec:"<<std::endl;
+        std::cout << board[badAnswers] << std::endl;
+        std::cout << std::endl;
+        std::cout << "Znane litery: " << displayLetters(knowLetters) << " (" << knowLetters.size() << "/" << chosenWord_letters.length() << ")" << std::endl;
+        std::cout << "Podaj słowo: ";
+        std::string word = "";
+        std::cin >> word;
+        for (auto letter: word) {
+            if (chosenWord_letters.contains(letter)) {
+                if (knowLetters.insert(letter).second) {
+                    guess = true;
+                    newLetters+=letter;
+                }
+            }
+        }
+        if (!guess) {
+            badAnswers++;
+            std::cout << "W szukanym słowie nie ma wspólnych liter…";
+        }
+        else if (guess && (word != chosenWord)) {
+            badAnswers++;
+            std::cout << "Znalazłeś nowe litery: "<< displayLetters(newLetters) << std::endl;
+        }
+        else {
+            std::cout << "Brawo odgadłeś słowo!" << std::endl;
+            return;
+        }
+    }
+    std::cout << "Przegrałeś, szukanym słowem było: " << chosenWord << std::endl;
+}
 
 int main() {
-    auto result = zad2();
-    std::cout << result << std::endl;
+    zad3();
 }
