f47b4b77cc24a13da1558a72c1d0612598340efd
pawi1 <p.rutkow@o2.pl>
2026-09-23 14:00:38 +0200

    t19

---
+++ projektowanie-aplikacji-desktopowych/zad1/Calc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zad1
+{
+    internal class Calc
+    {
+        public static double? Calculate(double a, double b, string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    return a + b;
+                case "-":
+                    return a - b;
+                case "*":
+                    return a * b;
+                case "/":
+                    if (b == 0)
+                        throw new DivideByZeroException();
+                    return a / b;
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ projektowanie-aplikacji-desktopowych/zad1/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zad1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Dictionary<string, bool?> selectedOptions = new Dictionary<string, bool?>
+            {
+                ["+"] = addButton.IsChecked,
+                ["-"] = subtractButton.IsChecked,
+                ["*"] = multiplyButton.IsChecked,
+                ["/"] = divideButton.IsChecked
+            };
+            try
+            {
+                double number1Value = double.Parse(number1.Text);
+                double number2Value = double.Parse(number2.Text);
+                string result = "";
+                foreach (var state in selectedOptions)
+                {
+                    if (state.Value == true)
+                    {
+                        result += $"{number1Value}{state.Key}{number2Value} = {Calc.Calculate(number1Value, number2Value, state.Key)}\n";
+                    }
+                }
+                if (result != "")
+                {
+                    resultLabel.Content = $"Wynik:\n{result}";
+                }
+                else
+                {
+                    resultLabel.Content = "Nie wybrano żadnej operacji";
+                }
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show("Nie można dzielić przez zero");
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Podaj prawidłowe liczby.");
+            }
+        }
+    }
+}
